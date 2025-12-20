1280af70f28a87af212d2626b737fc7291be69dd
debian <thilking.k@northeastern.edu>
2025-12-19 21:43:03 -0800

    implemented file header extraction

--- source/regex.hxx
+++ source/regex.hxx
@@ -1,9 +1,20 @@
 #include <string>
+#include <optional>
+
 
 namespace coup {
 	
-	std::optional< std::string > extract_header(const std::string& include)
+	std::optional< std::string > extract_header(const std::string& line) 
 	{
+		if(line.empty()) { return {}; }
+		if(line.find("#include") == line.end()) { return {}; }
 
+		size_t l = line.find('"') + 1;
+		if(l == std::string::npos) { return {}; }
+		size_t r = line.rfind('"');
+		if(r == std::string::npos) { return {}; }
+
+		return line.substr(l, r - l);
 	}
+	
 }
