e0c595b5bc7df133524d9958cae7b9d79b3dc1f6
debian <thilking.k@northeastern.edu>
2025-12-21 03:55:11 -0800

    updated test dir

--- tests/test.cxx
+++
@@ -1,7 +0,0 @@
-#include "test.hxx"
-
-int square(int x) { return x * x; }
-
-int add(int a, int b) { return a + b; }
-
-int multiply(int a, int b) { return a * b; }
--- tests/test.hxx
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-int square(int x);
-int add(int a, int b);
-int multiply(int a, int b);
