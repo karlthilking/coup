9602d99ecb9abf02ed1639ee61648c3aa0b627c5
karl <thilking.k@northeastern.edu>
2025-12-24 12:49:36 -0800

    added tests for all file tracker functions

--- tests/test_file_tracker.cxx
+++ tests/test_file_tracker.cxx
@@ -6,7 +6,8 @@
 #include <string_view>
 #include <iostream>
 
-#include "../source/file_tracker.hxx"
+#include "../include/file_tracker.hxx"
+#include "../include/parser.hxx"
 
 namespace fs = std::filesystem;
 using namespace coup;
@@ -17,44 +18,146 @@ protected:
 	file_tracker_test() = default;
 	~file_tracker_test() = default;
 		
-	file_tracker ft{};	
-
 	void SetUp() override {}
+  
+  fs::path s = "src/main.cpp";
+  fs::path h = "include/header.hpp";
+  fs::path o = "build/main.o";
 };
 
-TEST_F(file_tracker_test, test_root)
+TEST_F(file_tracker_test, get_root_test)
 {
-	fs::path root = ft.get_root();
-	EXPECT_FALSE(root.empty());
-	EXPECT_TRUE(fs::exists(root));
+  std::optional< fs::path > r_opt = get_root();
+  EXPECT_TRUE(r_opt.has_value());
 
-	std::cout << "Root path: " << root.string() << "\n";
+  fs::path root = r_opt.value();
+  EXPECT_TRUE(fs::exists(root));
+  EXPECT_FALSE(root.empty());
+  
+  std::cout << "[ROOT]:\n" << root.string() << "\n";
 }
 
-TEST_F(file_tracker_test, test_file_discovery)
+TEST_F(file_tracker_test, get_src_dir_test)
 {
-	std::vector< fs::path > src_files = ft.get_source_files();
-	std::vector< fs::path > header_files = ft.get_header_files();
+  std::optional< fs::path > r_opt = get_root();
+  fs::path root = r_opt.value();
 
-	EXPECT_TRUE(!(src_files.empty() && header_files.empty()));
-	
-	std::cout << "Source files:\n";
-	for(const auto& src: src_files)
-	{
-		EXPECT_FALSE(src.empty());
-		EXPECT_TRUE(fs::exists(src));
-		
-		std::cout << src.string() << "\n";
-	}
+  std::optional< fs::path > src_dir_opt = get_src_dir(root);
+  EXPECT_TRUE(src_dir_opt.has_value());
+  
+  fs::path src_dir = src_dir_opt.value();
+  EXPECT_TRUE(fs::exists(src_dir));
+  EXPECT_FALSE(src_dir.empty());
 
-	std::cout << "Header files:\n";
-	for(const auto& header: header_files)
-	{
-		EXPECT_FALSE(header.empty());
-		EXPECT_TRUE(fs::exists(header));
-
-		std::cout << header.string() << "\n";
-	}
+  std::cout << "[SRC]:\n" << src_dir.string() << "\n";
 }
+
+TEST_F(file_tracker_test, get_include_dir_test)
+{
+  std::optional< fs::path > r_opt = get_root();
+  fs::path root = r_opt.value();
+
+  std::optional< fs::path > inc_dir_opt = get_include_dir(root);
+  EXPECT_TRUE(inc_dir_opt.has_value());
+
+  fs::path include_dir = inc_dir_opt.value();
+  EXPECT_TRUE(fs::exists(include_dir));
+  EXPECT_FALSE(include_dir.empty());
+
+  std::cout << "[INCLUDE]:\n" << include_dir.string() << "\n";
+}
+
+TEST_F(file_tracker_test, get_src_files_test)
+{
+  std::optional< fs::path > r_opt = get_root();
+  fs::path root = r_opt.value();
+
+  std::optional< fs::path > src_dir_opt = get_src_dir(root);
+  fs::path src_dir = src_dir_opt.value();
+
+  std::vector< fs::path > src_files = get_src_files(src_dir);
+  EXPECT_FALSE(src_files.empty());
+    
+  std::cout << "[Source Files]:\n";
+  for(const fs::path& src: src_files)
+  {
+    std::cout << src.string() << "\n";
+    EXPECT_TRUE(src.parent_path() == src_dir);
+    EXPECT_TRUE(fs::exists(src));
+  }
+}
+
+TEST_F(file_tracker_test, get_header_files_test)
+{
+  std::optional< fs::path > r_opt = get_root();
+  fs::path root = r_opt.value();
+
+  std::optional< fs::path > inc_dir_opt = get_include_dir(root);
+  fs::path inc_dir = inc_dir_opt.value();
+
+  std::vector< fs::path > header_files = get_header_files(inc_dir);
+  EXPECT_FALSE(header_files.empty());
+  
+  std::cout << "[Header Files]:\n";
+  for(const fs::path& header: header_files)
+  {
+    std::cout << header.string() << "\n";
+    EXPECT_TRUE(header.parent_path() == inc_dir);
+    EXPECT_TRUE(fs::exists(header));
+  }
+}
+
+TEST_F(file_tracker_test, get_obj_files_test)
+{
+  std::optional< fs::path > r_opt = get_root();
+  fs::path root = r_opt.value();
+
+  std::vector< fs::path > obj_files = get_obj_files(root);
+
+  std::cout << "[Object Files]:\n";
+  for(const fs::path& obj: obj_files)
+  { 
+    EXPECT_TRUE(fs::exists(obj));
+    std::cout << obj.string();
+  }
+}
+
+TEST_F(file_tracker_test, is_src_file_true_test)
+{
+  EXPECT_TRUE(is_src_file(s));
+}
+TEST_F(file_tracker_test, is_src_file_false_test)
+{
+  EXPECT_FALSE(is_src_file(h));
+  EXPECT_FALSE(is_src_file(o));
+}
+
+TEST_F(file_tracker_test, is_header_file_true_test)
+{
+  
+  EXPECT_TRUE(is_header_file(h));
+}
+
+TEST_F(file_tracker_test, is_header_file_false_test)
+{
+  EXPECT_FALSE(is_header_file(s));
+  EXPECT_FALSE(is_header_file(o));
+}
+
+TEST_F(file_tracker_test, is_obj_file_true_test)
+{
+  EXPECT_TRUE(is_obj_file(o));
+}
+
+TEST_F(file_tracker_test, is_obj_file_false_test)
+{
+  EXPECT_FALSE(is_obj_file(s));
+  EXPECT_FALSE(is_obj_file(h));
+}
+
+
+
+
+
 		
 
