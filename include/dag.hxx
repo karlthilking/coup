2bb9754d6d8bb6e5a5a13f734d774fd51c789b95
karl <thilking.k@northeastern.edu>
2025-12-24 19:35:44 -0800

    updated dag class and utilities

--- include/dag.hxx
+++ include/dag.hxx
@@ -2,38 +2,34 @@
 
 #include <vector>
 #include <filesystem>
-#include <string>
-#include <string_view>
-#include <unordered_map>
-#include <queue>
-#include <optional>
 
 namespace fs = std::filesystem;
 
 namespace coup
 {
+  enum class file_type { source, header, object };
+
+  struct node
+  {
+    file_type f_type;
+    fs::path f_path;
+    std::vector< node* > children;
+
+    node(file_type ft, const fs::path& fp) noexcept;
+    void insert(node* child) noexcept; 
+    void insert(file_type ft, const fs::path& fp) noexcept;
+    fs::file_time_type last_update() const noexcept;
+  };
+  
   class dag
   {
   private:
     std::vector< node* > nodes;
   public:
-    dag(std::vector< node* > S) noexcept;
+    dag(const std::vector< node* >& S) noexcept;
+    dag(std::vector< node* >&& S) noexcept;
     ~dag() noexcept;
     std::vector< node* > topological_sort() const noexcept;
   };
-
-  struct node
-  {
-    file_type type;
-    fs::path path;
-    std::vector< node* > children;
-
-    node(file_type ft, const fs::path& p);
-    void insert(node* child) noexcept; 
-    void insert(file_type ft, const fs::path& p) noexcept;
-    fs::file_time_type last_update() const noexcept;
-  };
-  
-  enum class file_type { src, header, obj };
-  
+ 
 }
--- src/dag.cxx
+++ src/dag.cxx
@@ -3,81 +3,88 @@
 #include <unordered_map>
 #include <queue>
 #include <cassert>
+#include <utility>
 
-#include "#../include/dag.hxx"
+#include "../include/dag.hxx"
 
-namespace coup;
 namespace fs = std::filesystem;
 
-dag::dag(std::vector< node* > S) noexcept
-  : nodes(S),
-{}
-
-dag::~dag() noexcept
+namespace coup
 {
-  for(node* u: nodes)
+  node::node(file_type ft, const fs::path& fp) noexcept
+   : f_type(ft), 
+     f_path(fp)
+  {}
+
+  void node::insert(node* v) noexcept
   {
-    assert(u != nullptr);
-    delete(u);
+   children.push_back(v);
   }
-}
 
-std::vector< fs::path > dag::topological_sort() const noexcept
-{
-  std::unordered_map< node*, int > indegree;
-  for(const node* u: nodes)
+  void node::insert(file_type ft, const fs::path& fp) noexcept
   {
-    for(node* v: u->children)
-    {
-      indegree[v]++;
+   children.emplace_back(new node(ft, fp));
+  }
+
+  fs::file_time_type node::last_update() const noexcept
+  {
+   return fs::last_write_time(f_path);
+  }
+
+  dag::dag(const std::vector< node* >& S) noexcept
+   : nodes(S)
+  {}
+
+  dag::dag(std::vector< node* >&& S) noexcept
+   : nodes(std::exchange(S, {}))
+  {}
+
+  dag::~dag() noexcept
+  {
+   for(node* u: nodes)
+   {
+     assert(u != nullptr);
+     delete(u);
+   }
+  }
+
+  std::vector< node* > dag::topological_sort() const noexcept
+  {
+   std::unordered_map< node*, int > indegree;
+   for(const node* u: nodes)
+   {
+     for(node* v: u->children)
+     {
+       ++indegree[v];
+     }
+   }
+
+   std::queue< node* > q;
+   for(node* u: nodes)
+   {
+     if(indegree[u] == 0)
+     {
+       q.push(u);
+     }
+   }
+   assert(!q.empty());
+
+   std::vector< node* > topological_order;
+   while(!q.empty())
+   {
+     node* u = q.front(); q.pop();
+     topological_order.push_back(u);
+
+     for(node* v: u->children)
+     {
+       int deps = --indegree[v];
+       if(deps == 0)
+       {
+         q.push(v);
+       }
+     }
     }
+    assert(topological_order.size() == nodes.size());
+    return topological_order;
   }
-
-  std::queue< node* > q;
-  for(node* u: nodes)
-  {
-    if(indegree[u] == 0)
-    {
-      q.push(u);
-    }
-  }
-  assert(!q.empty());
-
-  std::vector< node* > topological_order;
-  while(!q.empty())
-  {
-    node* u = q.front(); q.pop();
-    topological_order.push_back(u);
-
-    for(node* v: u->children)
-    {
-      int deps = --indegree[v];
-      if(deps == 0)
-      {
-        q.push(v);
-      }
-    }
-  }
-  assert(topological_order.size() == nodes.size());
-  return toplogical_order;
-}
-
-node::node(file_type ft, const fs::path& p) noexcept
-  : type(ft), 
-    path(p),
-{}
-
-void node::insert(node* child) noexcept
-{
-  children.push_back(child);
-}
-
-void node::insert(file_type ft, const fs::path& p) noexcept
-{
-  children.emplace_back(new node(ft, p));
-}
-
-fs::file_time_type node::last_update() const noexcept
-{
-  return fs::last_write_time(path);
 }
