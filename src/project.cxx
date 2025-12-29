21ec4c29c4e6f9f36bc9add3a67da3509da24352
karl <thilking.k@northeastern.edu>
2025-12-29 01:53:03 -0800

    implementing coup project multithreaded creational pattern

--- src/project.cxx
+++ src/project.cxx
@@ -2,20 +2,21 @@
 #include <vector>
 #include <filesystem>
 #include <utility>
+#include <thread>
 #include "../include/project.hxx"
 
 namespace fs = std::filesystem;
 
 namespace coup
 {
-  coupfile::coupfile(const fs::path& s, const fs::path& h, const fs::path& o)
+  coup_file::coup_file(const fs::path& s, const fs::path& h, const fs::path& o)
     : src_file(s), header_file(h), obj_file(o),
       src_exists(fs::exists(s)), 
       header_exists(fs::exists(h)),
       obj_exists(fs::exists(o))
   {}
 
-  coupfile::coupfile(fs::path&& s, fs::path&& h, fs::path&& o)
+  coup_file::coup_file(fs::path&& s, fs::path&& h, fs::path&& o)
     : src_file(std::move(s)),
       header_file(std::move(h)),
       obj_file(std::move(o)),
@@ -24,12 +25,89 @@ namespace coup
       obj_exists(fs::exists(obj_file))
   {}
 
-  bool coupfile::src_exists() const noexcept { return src_exists; }
-  bool coupfile::header_exists() const noexcept { return header_exists; }
-  bool coupfile::obj_exists() const noexcept { return obj_exists; }
+  bool coup_file::src_exists() const noexcept { return src_exists; }
+  bool coup_file::header_exists() const noexcept { return header_exists; }
+  bool coup_file::obj_exists() const noexcept { return obj_exists; }
 
-  const fs::path& coupfile::get_src() const noexcept { return src_file; }
-  const fs::path& coupfile::get_header() const noexcept { return header_file; }
-  const fs::path& coupfile::get_obj() const noexcept { return obj_file; }
+  const fs::path& coup_file::get_src() const noexcept { return src_file; }
+  const fs::path& coup_file::get_header() const noexcept { return header_file; }
+  const fs::path& coup_file::get_obj() const noexcept { return obj_file; }
 
-  bool 
+  bool coup_file::requires_rebuild() const noexcept
+  {
+    assert(src_exists());
+    if(fs::last_write_time(obj_file) < fs::last_write_time(src_file) ||
+       fs::last_write_time(obj_file) < fs::last_write_time(header_file))
+    {
+      return true;
+    }
+    return false;
+  }
+
+  coup_project::create_project()
+  {
+    std::optional< fs::path > r_opt = coup::get_root();
+    if(!r_opt.has_value())
+    {
+      throw std::runtime_error("[ERROR] No root directory could be identified.\n");
+    }
+    fs::path root = r_opt.value();
+    
+    std::vector< fs::path > src_files;
+    std::vector< fs::path > header_files;
+    std::vector< fs::path > obj_files;
+    std::unordered_map< std::string, std::array< fs::path, 3 >> fgroups;
+
+    std::thread src_handler([&](){
+      std::optional< fs::path > s_opt = coup::get_src_dir(root);
+      if(!s_opt.has_value()) { return; }
+      src_files = coup::get_src_files(s_opt.value());
+      
+      for(const fs::path& src: src_files)
+      {
+        std::string src_name = src.string();
+        std::string_view src_stem = coup::get_stem(src_name);
+        fgroups[src_stem][0] = src;
+      }
+    });
+
+    std::thread header_handler([&](){
+      std::optional< fs::path > i_opt = coup::get_include_dir(root);
+      if(!i_opt.has_value()) { return; }
+      header_files = coup::get_header_files(i_opt.value());
+
+      for(const fs::path& header: header_files)
+      {
+        std::string header_name = header.string();
+        std::string_view header_stem = coup::get_stem(header_name);
+        fgroups[header_stem][1] = header;
+      }
+    });
+
+    std::thread obj_handler([&](){
+      obj_files = coup::get_obj_files(root);
+      
+      for(const fs::path& obj: obj_files)
+      {
+        std::string obj_name = obj.string();
+        std::string_view obj_stem = coup::get_stem(obj_name);
+        fgroups[obj_stem][2] = obj;
+      }
+    });
+    
+    src_handler.join();
+    header_handler.join();
+    obj_handler.join();
+    
+    std::vector< coup_file > cfiles;
+    for(const auto& [name, files]: fgroups)
+    {
+      assert(!files.empty());
+      cfiles.emplace_back(files[0], files[1], files[2]);
+    }
+    
+    return coup_project(cfiles);
+  }
+
+  std::vector< coup_file > coup_project::get_files() const noexcept { return files; }
+}
