cc891ee31e4b2db91af1375fc511f2857e2beb0a
karl <thilking.k@northeastern.edu>
2025-12-30 18:01:01 -0800

    formatted file tracker header

--- include/file_tracker.hxx
+++ include/file_tracker.hxx
@@ -1,217 +1,203 @@
 #pragma once
 
+#include <cassert>    // assert
 #include <filesystem> // path, last_write_time, current_path, parent_path, exists
-#include <vector>
-#include <string_view>
+#include <iostream>   // cerr
 #include <optional>
-#include <cassert> // assert
-#include <iostream> // cerr
+#include <string_view>
+#include <vector>
 
-#include "regex.hxx" // extract_header
 #include "parser.hxx" // get_stem, get_extension
+#include "regex.hxx"  // extract_header
 
-namespace fs = std::filesystem; 
-namespace coup 
-{
-  // discovers project root directory
-  // returns root directory path or empty if no root can be found
-  // a root is defined as a directory with either a src or include directory
-  inline std::optional< fs::path > get_root(const fs::path& p = fs::current_path())
-  {
-    fs::path cur = fs::absolute(p);
-    assert(fs::exists(p));
+namespace fs = std::filesystem;
+namespace coup {
+// discovers project root directory
+// returns root directory path or empty if no root can be found
+// a root is defined as a directory with either a src or include directory
+inline std::optional<fs::path>
+get_root(const fs::path &p = fs::current_path()) {
+  fs::path cur = fs::absolute(p);
+  assert(fs::exists(p));
 
-    for(;;)
-    {
-      if(fs::exists(cur / "src") || fs::exists(cur / "include"))
-      {
-        return cur;
-      }
-      fs::path next = cur.parent_path();
-      if(cur == next) { break; }
-      cur = next;
+  for (;;) {
+    if (fs::exists(cur / "src") || fs::exists(cur / "include")) {
+      return cur;
     }
-    return std::nullopt;
-  }
-  
-  // discovers source directory from project root
-  // returns src directory path or empty if no source directory exists
-  inline std::optional< fs::path > get_src_dir(const fs::path& root)
-  {
-    if(fs::exists(root / "src"))
-    {
-      return fs::path(root / "src");
+    fs::path next = cur.parent_path();
+    if (cur == next) {
+      break;
     }
-    return std::nullopt;
+    cur = next;
   }
-  
-  // discovers include directory from project root
-  // returns include directory path or empty if no include directory exists
-  inline std::optional< fs::path > get_include_dir(const fs::path& root)
-  {
-    if(fs::exists(root / "include"))
-    {
-      return fs::path(root / "include");
-    }
-    return std::nullopt;
-  }
-
-  // evaluates if a std::filesystem::path is a source file
-  [[nodiscard]] inline bool is_src_file(const fs::path& file)
-  {
-    std::string filepath = file.string();
-    std::optional< std::string_view > ext_opt = get_extension(filepath);
-
-    if(!ext_opt.has_value()) { return false; }
-    
-    std::string_view file_ext = ext_opt.value();
-	  if(file_ext == "cpp" ||
-			 file_ext == "cc" ||
-       file_ext == "cxx" ||
-			 file_ext == "C" ||
-			 file_ext == "c++")
-		{
-			return true;
-		}
-		return false;
-	}
-  
-  // evaluates if a std::filesystem::path is a header file
-  [[nodiscard]] inline bool is_header_file(const fs::path& file)
-  {
-    std::string filepath = file.string();
-    std::optional< std::string_view > ext_opt = get_extension(filepath);
-
-    if(!ext_opt.has_value()) { return false; }
-    
-    std::string_view file_ext = ext_opt.value();
-		if(file_ext == "h" ||
-			 file_ext == "hh" ||
-			 file_ext == "hpp" ||
-			 file_ext == "hxx" ||
-			 file_ext == "h++" ||
-			 file_ext == "H")
-		{
-			return true;
-		}
-    return false;
-	}
-  
-  // evaluates if a std::filesystem::path is an object file
-  [[nodiscard]]
-  inline bool is_obj_file(const fs::path& file)
-  {
-    std::string filepath = file.string();
-    std::optional< std::string_view > ext_opt = get_extension(filepath);
-
-    if(!ext_opt.has_value()) { return false; }
-
-    std::string_view file_ext = ext_opt.value();
-    if(file_ext == "o" || file_ext == "obj") { return true; }
-    return false;
-  }
-	
-  // discover all source files from project src directory
-	[[nodiscard]]
-  inline std::vector< fs::path > get_src_files(const fs::path& src_dir)
-  {
-    std::vector< fs::path > src_files;
-
-    for(const auto& entry: fs::recursive_directory_iterator(src_dir))
-    {
-      if(!entry.is_regular_file()) { continue; }
-      
-      fs::path file_path = entry.path();
-      if(file_path.empty()) { continue; }
-      
-      if(is_src_file(file_path))
-      {
-        src_files.push_back(file_path);
-      }
-      else if(is_header_file(file_path))
-      {
-        std::optional< std::string_view > fn_opt  = get_filename(file_path.string());
-        assert(fn_opt.has_value());
-
-        std::string_view filename = fn_opt.value();
-        std::cerr << "[WARNING] Header file in src directory: "
-          << filename << "\n";
-      }
-    }
-    return src_files;
-  }
-  
-  // discover all header files from project include directory
-  [[nodiscard]] 
-  inline std::vector< fs::path > get_header_files(const fs::path& include_dir)
-  {
-    std::vector< fs::path > header_files;
-
-    for(const auto& entry: fs::recursive_directory_iterator(include_dir))
-    {
-      if(!entry.is_regular_file()) { continue; }
-
-      fs::path file_path = entry.path();
-      if(file_path.empty()) { continue; }
-      
-      if(is_header_file(file_path))
-      {
-        header_files.push_back(file_path);
-      }
-      else if(is_src_file(file_path))
-      {
-        std::optional< std::string_view > fn_opt = get_filename(file_path.string());
-        assert(fn_opt.has_value());
-
-        std::string_view filename = fn_opt.value();
-        std::cerr << "[WARNING] Source file in include/: "
-          << filename << "\n";
-      }
-    }
-    return header_files;
-  }
-  
-  // discover all object files descending from project root
-  [[nodiscard]]
-  inline std::vector< fs::path > get_obj_files(const fs::path& root)
-  { 
-    std::vector< fs::path > obj_files;
-    for(const auto& entry: fs::recursive_directory_iterator(root))
-    {
-      if(!entry.is_regular_file()) { continue; }
-
-      fs::path cur = entry.path();
-      if(cur.empty()) { continue; }
-      
-      if(is_obj_file(cur))
-      {
-        obj_files.push_back(cur);
-      }
-    }
-    return obj_files;
-  }
-
-  // std::filesystem::absolute wrapper function
-  inline fs::path get_full_path(std::string_view filename)
-  {
-    return fs::absolute(fs::path(filename));
-  }
-  
-  // get absolute path of source file in src directory
-  inline fs::path get_abs_src(std::string_view src_name,
-                              const fs::path& src_dir)
-  {
-    fs::path abs_src_path(src_dir / src_name);
-    assert(fs::exists(abs_src_path));
-    return abs_src_path;
-  }
-  
-  // get absolute path of header file in include directory
-  inline fs::path get_abs_header(std::string_view header_name,
-                                 const fs::path& include_dir)
-  {
-    fs::path abs_header_path(include_dir / header_name);
-    assert(fs::exists(abs_header_path));
-    return abs_header_path;
-  }
+  return std::nullopt;
 }
+
+// discovers source directory from project root
+// returns src directory path or empty if no source directory exists
+inline std::optional<fs::path> get_src_dir(const fs::path &root) {
+  if (fs::exists(root / "src")) {
+    return fs::path(root / "src");
+  }
+  return std::nullopt;
+}
+
+// discovers include directory from project root
+// returns include directory path or empty if no include directory exists
+inline std::optional<fs::path> get_include_dir(const fs::path &root) {
+  if (fs::exists(root / "include")) {
+    return fs::path(root / "include");
+  }
+  return std::nullopt;
+}
+
+// evaluates if a std::filesystem::path is a source file
+[[nodiscard]] inline bool is_src_file(const fs::path &file) {
+  std::string filepath = file.string();
+  std::optional<std::string_view> ext_opt = get_extension(filepath);
+
+  if (!ext_opt.has_value()) {
+    return false;
+  }
+
+  std::string_view file_ext = ext_opt.value();
+  if (file_ext == "cpp" || file_ext == "cc" || file_ext == "cxx" ||
+      file_ext == "C" || file_ext == "c++") {
+    return true;
+  }
+  return false;
+}
+
+// evaluates if a std::filesystem::path is a header file
+[[nodiscard]] inline bool is_header_file(const fs::path &file) {
+  std::string filepath = file.string();
+  std::optional<std::string_view> ext_opt = get_extension(filepath);
+
+  if (!ext_opt.has_value()) {
+    return false;
+  }
+
+  std::string_view file_ext = ext_opt.value();
+  if (file_ext == "h" || file_ext == "hh" || file_ext == "hpp" ||
+      file_ext == "hxx" || file_ext == "h++" || file_ext == "H") {
+    return true;
+  }
+  return false;
+}
+
+// evaluates if a std::filesystem::path is an object file
+[[nodiscard]]
+inline bool is_obj_file(const fs::path &file) {
+  std::string filepath = file.string();
+  std::optional<std::string_view> ext_opt = get_extension(filepath);
+
+  if (!ext_opt.has_value()) {
+    return false;
+  }
+
+  std::string_view file_ext = ext_opt.value();
+  if (file_ext == "o" || file_ext == "obj") {
+    return true;
+  }
+  return false;
+}
+
+// discover all source files from project src directory
+[[nodiscard]]
+inline std::vector<fs::path> get_src_files(const fs::path &src_dir) {
+  std::vector<fs::path> src_files;
+
+  for (const auto &entry : fs::recursive_directory_iterator(src_dir)) {
+    if (!entry.is_regular_file()) {
+      continue;
+    }
+
+    fs::path file_path = entry.path();
+    if (file_path.empty()) {
+      continue;
+    }
+
+    if (is_src_file(file_path)) {
+      src_files.push_back(file_path);
+    } else if (is_header_file(file_path)) {
+      std::optional<std::string_view> fn_opt = get_filename(file_path.string());
+      assert(fn_opt.has_value());
+
+      std::string_view filename = fn_opt.value();
+      std::cerr << "[WARNING] Header file in src directory: " << filename
+                << "\n";
+    }
+  }
+  return src_files;
+}
+
+// discover all header files from project include directory
+[[nodiscard]]
+inline std::vector<fs::path> get_header_files(const fs::path &include_dir) {
+  std::vector<fs::path> header_files;
+
+  for (const auto &entry : fs::recursive_directory_iterator(include_dir)) {
+    if (!entry.is_regular_file()) {
+      continue;
+    }
+
+    fs::path file_path = entry.path();
+    if (file_path.empty()) {
+      continue;
+    }
+
+    if (is_header_file(file_path)) {
+      header_files.push_back(file_path);
+    } else if (is_src_file(file_path)) {
+      std::optional<std::string_view> fn_opt = get_filename(file_path.string());
+      assert(fn_opt.has_value());
+
+      std::string_view filename = fn_opt.value();
+      std::cerr << "[WARNING] Source file in include/: " << filename << "\n";
+    }
+  }
+  return header_files;
+}
+
+// discover all object files descending from project root
+[[nodiscard]]
+inline std::vector<fs::path> get_obj_files(const fs::path &root) {
+  std::vector<fs::path> obj_files;
+  for (const auto &entry : fs::recursive_directory_iterator(root)) {
+    if (!entry.is_regular_file()) {
+      continue;
+    }
+
+    fs::path cur = entry.path();
+    if (cur.empty()) {
+      continue;
+    }
+
+    if (is_obj_file(cur)) {
+      obj_files.push_back(cur);
+    }
+  }
+  return obj_files;
+}
+
+// std::filesystem::absolute wrapper function
+inline fs::path get_full_path(std::string_view filename) {
+  return fs::absolute(fs::path(filename));
+}
+
+// get absolute path of source file in src directory
+inline fs::path get_abs_src(std::string_view src_name,
+                            const fs::path &src_dir) {
+  fs::path abs_src_path(src_dir / src_name);
+  assert(fs::exists(abs_src_path));
+  return abs_src_path;
+}
+
+// get absolute path of header file in include directory
+inline fs::path get_abs_header(std::string_view header_name,
+                               const fs::path &include_dir) {
+  fs::path abs_header_path(include_dir / header_name);
+  assert(fs::exists(abs_header_path));
+  return abs_header_path;
+}
+} // namespace coup
